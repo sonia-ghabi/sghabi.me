5491522e16c80c074f19f5ffe8b7fd09018ff952
Sonia Ghabi <sonia.ghabi@gmail.com>
2016-02-13 17:45:47 -0500

    First window and file opening

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,47 @@
-var console = document.getElementById("terminal");
-var consoleInput = document.getElementById("terminalCommand"); 
-var consoleOutput = document.getElementById("terminalDisplay");
+var terminal = document.getElementById("terminal");
+var terminalWindow = document.getElementById("terminalWindow");
+var workFolderWindow = document.getElementById("workFolder");
+var terminalInput = document.getElementById("terminalCommand"); 
+var terminalOutput = document.getElementById("terminalDisplay");
+var icon = document.getElementById("icon");
+var closeButtons = document.getElementsByClassName("closeButton");
 
-console.addEventListener("click", function(event){
-    consoleInput.focus();
+var resumeIcon = document.getElementById("resumeIcon");
+var resumeWindow = document.getElementById("resumeWindow");
+
+resumeIcon.addEventListener("dblclick", function(event) {
+    document.getElementById("resumeWindow").className= "frame";
 });
+
+icon.addEventListener("dblclick", function(event) {
+    workFolderWindow.className = "drawWindow"; 
+});
+
+for(var button of closeButtons) {
+    button.addEventListener("click", function(event) {
+        console.log(document.getElementById(event.target.dataset.close));
+        document.getElementById(event.target.dataset.close).className = "hidden";
+    });
+}
+
+
+terminal.addEventListener("click", function(event){
+    terminalInput.focus();
+});
+
+
     
 /**
  * This object takes an input element, an output elements and a command list
  * and creates a simulated 
  * 
- * @param input    [Element] console's input element (usually an input)
- * @param output   [Element] console's output (usually a pre)
+ * @param input    [Element] terminal
+'s input element (usually an input)
+ * @param output   [Element] terminal
+'s output (usually a pre)
  * @param commands [Map]     a map containing all commands and it's outputs
  */
-var consoleManager = function (input, output, commands){
+var terminalManager = function (input, output, commands){
     input.addEventListener("keyup", function (event){
         var keyCode = event.keyCode || event.which;
         if (keyCode == '13'){
@@ -42,12 +69,7 @@ commands.set("help", showHelp);
 commands.set("contact", showContact);
 
 function showWelcome(param) {
-return "██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗\n"+
-"██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝\n"+
-"██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗\n"+  
-"██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝\n"+  
-"╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗\n"+
-" ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝\n";
+return "Last login: Fri Feb 12 11:55:12 on ttys000 \n";
 }
 
 function showHelp(param) {
@@ -55,7 +77,36 @@ function showHelp(param) {
 }
     
 function showContact(param) {
+    return "aaaa";
     //openContactPopup();
 }
 
-consoleManager(consoleInput, consoleOutput, commands);
+terminalManager(terminalInput, terminalOutput, commands);
+
+// Drag'n'drop
+
+function drag_start(event) {
+    if(event.target.draggable) {
+        var style = window.getComputedStyle(event.target, null);
+        event.dataTransfer.setData("text/plain", event.target.id + ',' +
+        (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
+    }
+} 
+
+function drop(event) {
+    var data = event.dataTransfer.getData("text/plain").split(',');
+    document.getElementById(data[0]).style.left = (event.clientX + parseInt(data[1],10)) + 'px';
+    document.getElementById(data[0]).style.top = (event.clientY + parseInt(data[2],10)) + 'px';
+    event.preventDefault();
+    return false;
+}
+
+function drag_over(event) {
+    event.preventDefault();
+    return false;
+} 
+
+document.body.addEventListener('dragstart',drag_start,false);
+document.body.addEventListener('dragover',drag_over,false);
+document.body.addEventListener('drop',drop,false); 
+
